12267e36099d2058fa614baeab52d7c7860ba871
leskevich <leskevichtema@gmail.com>
2023-04-01 17:01:12 +0300

    fix: eslintrc.json

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
+
   return (
     <>
-
+      <h1 className={"sdcard"}>
+        {"dsafdsascdsssssssdsafdsascdssssssssdsaf dsascdssssssssdsafdsassdcsdcdscdssssssssdsafdsascdssssssss"}
+      </h1>
     </>
   )
 }
