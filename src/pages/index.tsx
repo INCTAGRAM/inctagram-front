56b4e6a39af685bacbb62643223db63030c3556f
leskevich <leskevichtema@gmail.com>
2023-04-02 21:42:05 +0300

    fix: eslinter and pretter

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
 export default function Home() {
   return (
     <>
-      <h1 className={"sdcard"}>
-        {
-          "dsafdsascdsssssssdsafdsascdssssssssdsaf dsascdssssssssdsafdsassdcsdcdscdssssssssdsafdsascdssssssss"
-        }
+      <h1 className={'sdcard'}>
+        {'dsafdsascdsssssssdsafdsascdssssssssdsaf dsascdssssssssdsafdsassdcsdcdscdssssssssdsafdsascdssssssss'}
       </h1>
     </>
-  );
+  )
 }
