2715262104fc4a1dfd74d3f893df9e37785a8e79
Timothy <redkovtim@gmail.com>
2023-04-05 21:25:50 +0100

    react-query mutation request init

--- src/common/ui/Form/Form.tsx
+++ src/common/ui/Form/Form.tsx
@@ -3,10 +3,11 @@ import { FieldValues, SubmitHandler, useForm, UseFormReturn } from 'react-hook-f
 
 type FormProps<TFormValues extends FieldValues> = {
   onSubmit: SubmitHandler<TFormValues>
+  LoginSchema: any
   children: (method: UseFormReturn<TFormValues>) => React.ReactNode
 }
 
-export const Form = <TFormValues extends FieldValues>({ onSubmit, children }: FormProps<TFormValues>) => {
+export const Form = <TFormValues extends FieldValues>({ onSubmit, LoginSchema, children }: FormProps<TFormValues>) => {
   const method = useForm<TFormValues>()
   return <form onSubmit={method.handleSubmit(onSubmit)}>{children(method)}</form>
 }
