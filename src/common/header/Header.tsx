088af7ada4893a857aca3eaf99c82571de46834e
leskevich <leskevichtema@gmail.com>
2023-04-13 15:32:38 +0300

    feat: add header

--- src/common/header/Header.tsx
+++ src/common/header/Header.tsx
@@ -1,9 +1,17 @@
+import style from './Header.module.scss'
+import Image from 'next/image'
+import Inctagram from './../../assets/image/Inctagram.svg'
+
 interface IHeader {
   logout: boolean
 }
 
 const Header = ({ logout }: IHeader) => {
-  return <>Header</>
+  return (
+    <div className={style.headerContainer}>
+      <Image src={Inctagram} alt={'logo'} className={style.logo} />
+    </div>
+  )
 }
 
 export default Header
