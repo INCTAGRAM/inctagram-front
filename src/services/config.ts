4430fbfa94b59ef02fcb2ba75583ad51c23a1650
VictoriaKirplyuk <meosh99@bk.ru>
2023-04-15 16:08:40 +0300

    feat: added request interceptor with access token

--- src/services/config.ts
+++ src/services/config.ts
@@ -3,3 +3,11 @@ import axios from 'axios'
 export const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
 })
+
+instance.interceptors.request.use((config) => {
+  const accessToken = localStorage.getItem('accessToken')
+
+  if (accessToken) config.headers.Authorization = `Bearer ${accessToken}`
+
+  return config
+})
