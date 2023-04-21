f04231654a8e0a72402175d45c7c3b50a19e35be
Timothy <redkovtim@gmail.com>
2023-04-21 09:06:00 +0100

    refactor: changeProfileSchema

--- src/validations/profile-schemes.ts
+++ src/validations/profile-schemes.ts
@@ -1,9 +1,9 @@
 import * as yup from 'yup'
 
 export const changeProfileSchema = yup.object().shape({
-  name: yup.string().required(),
-  surname: yup.string().required(),
-  birthday: yup.date().required(),
-  city: yup.string().required(),
-  aboutMe: yup.string().required(),
+  name: yup.string().min(1).max(40),
+  surname: yup.string().min(1).max(40),
+  birthday: yup.date(),
+  city: yup.string().min(1).max(60),
+  aboutMe: yup.string().min(1).max(200),
 })
