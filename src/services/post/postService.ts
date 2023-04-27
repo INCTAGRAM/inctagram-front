c553d59b76d04e129145b7dae8603ecda05e320a
leskevich <leskevichtema@gmail.com>
2023-04-27 19:10:33 +0300

    feet: postService.ts

---
+++ src/services/post/postService.ts
@@ -0,0 +1,14 @@
+import { instance } from '@/services/config'
+import { TChangePost, TResponseCreatePost } from '@/services/post/type'
+
+export const postService = {
+  createPost: (payload: string) => {
+    return instance.post<TResponseCreatePost>('/users/self/posts', { payload })
+  },
+  removePost: (id: string) => {
+    return instance.delete(`/users/self/posts/${id}`)
+  },
+  changeDescriptionPost: (payload: TChangePost) => {
+    return instance.patch(`/users/self/posts/${payload.id}`, payload.description)
+  },
+}
