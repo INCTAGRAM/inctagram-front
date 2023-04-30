dc199ebba4b4109fae8b88c4696cc7ff4f18c619
Daniil Fefelov <fefelov.danil@gmail.com>
2023-05-01 00:54:42 +0400

    feat: added aspect for cropping

--- src/features/popups/createPostPopup/croppingPhotoPopup/CroppingPhotoPopup.tsx
+++ src/features/popups/createPostPopup/croppingPhotoPopup/CroppingPhotoPopup.tsx
@@ -1,10 +1,11 @@
 import styles from './CroppingPhotoPopup.module.scss'
 import { Popup } from '@/common/ui/popup/Popup'
 import Cropper, { Area } from 'react-easy-crop'
-import { useState } from 'react'
+import React, { useState } from 'react'
 import { CroppedAreaType } from '@/features/popups/addPhotoPopup/body/bodySavePhotoPopup/BodySavePhotoPopup'
 import { IPost } from '@/features/popups/createPostPopup/types'
 import { generateDownload } from '@/utils'
+import { ControlElement } from '@/features/popups/createPostPopup/croppingPhotoPopup/controlElement/ControlElement'
 
 interface ICroppingPhotoPopup {
   post: IPost
@@ -25,7 +26,6 @@ export const CroppingPhotoPopup = ({
   const [crop, setCrop] = useState({ x: 0, y: 0 })
   const [zoom, setZoom] = useState(1)
   const [aspect, setAspect] = useState(1)
-  const [testImg, setTestImg] = useState('')
 
   const onCropComplete = (croppedAreaPercentage: Area, croppedAreaPixels: Area) => {
     setCroppedArea(croppedAreaPixels)
@@ -70,6 +70,21 @@ export const CroppingPhotoPopup = ({
             />
           )
         })}
+        <ControlElement icon={'expand-outline'} elementClass={'aspect'}>
+          <div className={`${styles.popupControlElement} ${styles.aspectControlElement}`}>
+            <p className={styles.sides1_1} onClick={() => setAspect(1)}>
+              1:1
+            </p>
+            <p className={styles.sides4_5} onClick={() => setAspect(4 / 5)}>
+              4:5
+            </p>
+            <p className={styles.sides16_9} onClick={() => setAspect(16 / 9)}>
+              16:9
+            </p>
+          </div>
+        </ControlElement>
+        <ControlElement icon={'maximize-outline'} elementClass={'zoom'}></ControlElement>
+        <ControlElement icon={'image-outline'} elementClass={'gallery'}></ControlElement>
       </div>
     </Popup>
   )
