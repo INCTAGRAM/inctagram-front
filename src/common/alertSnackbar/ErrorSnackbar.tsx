8a2d00845abf421e2d9f08a7f6682937c12df6d9
Daniil Fefelov <fefelov.danil@gmail.com>
2023-05-08 22:35:20 +0400

    refactor: swapped AlertSnackbar.tsx for the ErrorSnackbar.tsx

--- src/common/alertSnackbar/ErrorSnackbar.tsx
+++ src/common/alertSnackbar/ErrorSnackbar.tsx
@@ -1,12 +1,10 @@
 import { forwardRef, SyntheticEvent, useState } from 'react'
 import MuiAlert, { AlertProps } from '@mui/material/Alert'
 import Snackbar, { SnackbarCloseReason } from '@mui/material/Snackbar'
-import { AxiosError } from 'axios'
-import { errorHandler } from '@/hooks/errorsHandler'
+import { IErrorResponse } from '@/services/auth/types'
 
 interface IAlertSnackbar {
-  type: 'error' | 'success' | null
-  error: AxiosError | string
+  error: string | IErrorResponse | undefined
   time?: number
 }
 
@@ -14,14 +12,16 @@ const Alert = forwardRef<HTMLDivElement, AlertProps>(function Alert(props, ref) 
   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />
 })
 
-export function AlertSnackbar({ error, type, time }: IAlertSnackbar) {
+export function ErrorSnackbar({ error, time }: IAlertSnackbar) {
   const [open, setOpen] = useState(true)
   let message
 
-  if (typeof error !== 'string') {
-    message = errorHandler(error)
-  } else {
+  if (typeof error === 'string') {
     message = error
+  } else if (error?.data) {
+    message = error.data.message[0]
+  } else {
+    message = 'Unknown error'
   }
 
   const handleClose = (event: Event | SyntheticEvent, reason?: SnackbarCloseReason) => {
@@ -33,7 +33,7 @@ export function AlertSnackbar({ error, type, time }: IAlertSnackbar) {
 
   return (
     <Snackbar open={open} autoHideDuration={time ? time : 6000} onClose={handleClose}>
-      <Alert onClose={handleClose} severity={type || 'success'} sx={{ width: '100%' }}>
+      <Alert onClose={handleClose} severity={'error'} sx={{ width: '100%' }}>
         {message}
       </Alert>
     </Snackbar>
