6a7661f1c70b5f1b1433da65787cf8cbe829d5d6
Daniil Fefelov <fefelov.danil@gmail.com>
2023-05-21 22:05:56 +0400

    refactor: ProfileInfo.tsx

--- src/features/profile/profileInfo/additionalInfo/AdditionalInfo.tsx
+++ src/features/profile/profileInfo/additionalInfo/AdditionalInfo.tsx
@@ -1,31 +1,36 @@
-import styles from './AdditionalInfo.module.scss'
-import { formatDateOfBirth } from '@/utils'
 import { Nullable } from '@/common/types/Nullable'
+import styles from './AdditionalInfo.module.scss'
+import iconSet from '@/assets/icons/selection.json'
+import IcomoonReact from 'icomoon-react'
 
 interface IAdditionalInfoProps {
-  city: Nullable<string>
+  name: Nullable<string>
+  surname: Nullable<string>
   birthday: Nullable<string>
+  city: Nullable<string>
 }
 
-const AdditionalInfo = ({ city, birthday }: IAdditionalInfoProps) => {
-  const isAdditionalInfo = [city, birthday].some((info) => !!info)
-
-  return isAdditionalInfo ? (
+export const AdditionalInfo = ({ name, surname, birthday, city }: IAdditionalInfoProps) => {
+  return (
     <div className={styles.additionalInfo}>
+      {(name || surname) && (
+        <div className={styles.infoAndIcon}>
+          <IcomoonReact icon={'person-outline'} iconSet={iconSet} color={'#ccc'} size={18} />
+          <p className={styles.name}>{`${name} ${surname}`}</p>
+        </div>
+      )}
       {birthday && (
-        <div className={styles.additionalInfo_item}>
-          Date of Birth: <span>{formatDateOfBirth(birthday)}</span>
+        <div className={styles.infoAndIcon}>
+          <IcomoonReact icon={'calendar-outline'} iconSet={iconSet} color={'#ccc'} size={18} />
+          <p className={styles.birthday}>{birthday}</p>
         </div>
       )}
       {city && (
-        <div className={styles.additionalInfo_item}>
-          City: <span>{city}</span>
+        <div className={styles.infoAndIcon}>
+          <IcomoonReact icon={'pin-outline'} iconSet={iconSet} color={'#ccc'} size={18} />
+          <p className={styles.city}>{city}</p>
         </div>
       )}
     </div>
-  ) : (
-    <div className={styles.no_content}>No information</div>
   )
 }
-
-export default AdditionalInfo
