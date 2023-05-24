be203e63035b4d464f8b14b844f1380fd629e667
Daniil Fefelov <fefelov.danil@gmail.com>
2023-05-24 14:46:25 +0400

    delete: react-query dependencies

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,7 @@
 import '@/assets/styles/reset.css'
 import '@/assets/styles/globals.scss'
 import type { AppProps } from 'next/app'
-import { Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import React, { ReactElement, ReactNode, useState } from 'react'
+import React, { ReactElement, ReactNode } from 'react'
 import { NextPage } from 'next'
 import { useLoader } from '@/hooks/useLoader'
 import '@/assets/styles/nprogress.css'
@@ -20,25 +18,18 @@ type AppPropsWithLayout = AppProps & {
 }
 
 export default function App({ Component, pageProps, ...rest }: AppPropsWithLayout) {
-  const [queryClient] = useState(() => new QueryClient())
-
   useLoader()
 
   const getLayout = Component.getLayout ?? ((page) => page)
   const { store } = wrapper.useWrappedStore(rest)
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <Hydrate state={pageProps.dehydratedState}>
-        <Provider store={store}>
-          {getLayout(
-            <Redirect>
-              <Component {...pageProps} />
-            </Redirect>
-          )}
-        </Provider>
-      </Hydrate>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <Provider store={store}>
+      {getLayout(
+        <Redirect>
+          <Component {...pageProps} />
+        </Redirect>
+      )}
+    </Provider>
   )
 }
